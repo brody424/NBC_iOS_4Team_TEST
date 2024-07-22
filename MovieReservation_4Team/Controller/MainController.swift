f6a4906cf165c059de491340cc9aa7912434f080
jjoohee <leejh950417@naver.com>
2024-07-22 19:30:09 +0900

    [ bug ] 기초파일 수정본

---
+++ MovieReservation_4Team/Controller/MainController.swift
@@ -0,0 +1,8 @@
+//
+//  MainController.swift
+//  MovieReservation_4Team
+//
+//  Created by t2023-m0023 on 7/22/24.
+//
+
+import Foundation
---
+++ MovieReservation_4Team/Manage/Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Manager.swift
+//  MovieReservation_4Team
+//
+//  Created by t2023-m0023 on 7/22/24.
+//
+
+import Foundation
