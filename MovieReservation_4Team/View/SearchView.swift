24a23b162af67c9693311f35f2e8d6b41ed3affa
Jeom1028 <apam144@gmail.com>
2024-07-24 10:06:44 +0900

    feat: #7 - searchView MVC패턴으로 나눔

--- MovieReservation_4Team/View/SearchView.swift
+++ MovieReservation_4Team/View/SearchView.swift
@@ -1,9 +1,16 @@
+//
+//  SearchView.swift
+//  MovieReservation_4Team
+//
+//  Created by t2023-m0117 on 7/24/24.
+//
+
 import UIKit
 import SnapKit
 
-class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
+class SearchView: UIView {
     
-    private let searchLabel: UILabel = {
+    let searchLabel: UILabel = {
         let label = UILabel()
         label.text = "검 색"
         label.textColor = UIColor.mainWhite
@@ -11,7 +18,7 @@ class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableVie
         return label
     }()
     
-    private let searchBar: UISearchBar = {
+    let searchBar: UISearchBar = {
         let search = UISearchBar()
         search.placeholder = "영화 검색"
         search.barStyle = .default
@@ -39,7 +46,7 @@ class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableVie
         return search
     }()
     
-    private let recentSearchesLabel: UILabel = {
+    let recentSearchesLabel: UILabel = {
         let label = UILabel()
         label.text = "최근 검색"
         label.textColor = UIColor.mainWhite
@@ -47,24 +54,21 @@ class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableVie
         return label
     }()
     
-    private lazy var clearAllButton: UIButton = {
+    let clearAllButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("전체 삭제", for: .normal)
         button.setTitleColor(UIColor.gray, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-        button.addTarget(self, action: #selector(clearAllButtonTapped), for: .touchUpInside)
         return button
     }()
     
-    private let recentSearchesTableView: UITableView = {
+    let recentSearchesTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: "SearchTableViewCell")
         tableView.backgroundColor = UIColor.mainBlack
         tableView.separatorStyle = .none
         return tableView
     }()
-    
-    private var recentSearches: [String] = []
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -77,11 +81,7 @@ class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableVie
     
     private func configure() {
         self.backgroundColor = UIColor.mainBlack
-        
-        searchBar.delegate = self
-        recentSearchesTableView.dataSource = self
-        recentSearchesTableView.delegate = self
-        
+
         [searchLabel, searchBar, recentSearchesLabel, clearAllButton, recentSearchesTableView].forEach { self.addSubview($0) }
         
         searchLabel.snp.makeConstraints {
@@ -110,66 +110,6 @@ class SearchView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableVie
             $0.leading.equalToSuperview().offset(8)
             $0.trailing.equalToSuperview().offset(-8)
             $0.bottom.equalToSuperview().offset(-8)
-        }
-    }
-    
-    // MARK: - 서치바딜리게이트
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print("검색 텍스트: \(searchText)")
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let searchText = searchBar.text, !searchText.isEmpty {
-            if !recentSearches.contains(searchText) {
-                recentSearches.insert(searchText, at: 0)
-                recentSearchesTableView.reloadData()
-            }
-        }
-        searchBar.resignFirstResponder()
-    }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchBar.becomeFirstResponder()
-    }
-    
-    // MARK: - 테이블뷰 데이터소스
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recentSearches.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
-        cell.configure(with: recentSearches[indexPath.row])
-        cell.deleteButton.addTarget(self, action: #selector(deleteButtonTapped(_:)), for: .touchUpInside)
-        cell.deleteButton.tag = indexPath.row
-        return cell
-    }
-    
-    // MARK: - 테이블뷰 딜리게이트
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedSearch = recentSearches[indexPath.row]
-        searchBar.text = selectedSearch
-        searchBarSearchButtonClicked(searchBar) // Trigger a search
-        searchBar.resignFirstResponder()
-    }
-    
-    // MARK: - 전체 삭제
-    
-    @objc private func clearAllButtonTapped() {
-        recentSearches.removeAll()
-        recentSearchesTableView.reloadData()
-    }
-    
-    // MARK: - 개별 삭제
-    
-    @objc private func deleteButtonTapped(_ sender: UIButton) {
-        let index = sender.tag
-        if index >= 0 && index < recentSearches.count {
-            recentSearches.remove(at: index)
-            recentSearchesTableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
         }
     }
 }
@@ -220,8 +160,4 @@ class SearchTableViewCell: UITableViewCell {
     func configure(with text: String) {
         titleLabel.text = text
     }
-}
-#Preview {
-  let lc = SearchView()
-  return lc
 }
