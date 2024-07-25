e25d0c07d4a0bf6a0833f8878619c783f5cefd87
jihyeon <dkswlgus0314@naver.com>
2024-07-25 10:24:29 +0900

    feat: 예약하기 버튼 클릭 시 하프모달 로직 연결

---
+++ MovieReservation_4Team/Controller/ModalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModalViewController.swift
+//  MovieReservation_4Team
+//
+//  Created by ahnzihyeon on 7/25/24.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
--- MovieReservation_4Team/Controller/MovieInfoViewController.swift
+++ MovieReservation_4Team/Controller/MovieInfoViewController.swift
@@ -1,30 +1,79 @@
 import UIKit
-
 class MovieInfoViewController: UIViewController {
     
-    var movie: Movie?
-    let posterImageView = UIImageView()
+    var movieInfoView = MovieInfoView()
+    
+    //  let networkManager: NetworkManagerProtocol //같은 프로토콜 채택 중
+    //  init(networkManager: NetworkManagerProtocol) {
+    //    self.networkManager = networkManager
+    //  }
+    //
+    //  required init?(coder: NSCoder) {
+    //    fatalError("init(coder:) has not been implemented")
+    //  }
+    
+    override func loadView() {
+        view = movieInfoView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        movieInfoView.reservationButton.addTarget(self, action: #selector(showModal), for: .touchDown)
         
-        setupView()
-        if let movie = movie, let posterPath = movie.posterPath {
-            let imageUrl = "https://image.tmdb.org/t/p/w500\(posterPath)"
-            NetworkManager.shared.loadImage(from: imageUrl, into: posterImageView)
-        }
+        readMovieDetail()
     }
     
-    func setupView() {
-        view.backgroundColor = .white
-        view.addSubview(posterImageView)
-        
-        posterImageView.contentMode = .scaleAspectFill
-        posterImageView.clipsToBounds = true
-        
-        posterImageView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(200)
+    // [예약하기] 버튼 클릭 시 모달 띄우기
+    @objc
+    func showModal(){
+        let modalVc = ModalViewController()
+        if let modal = modalVc.sheetPresentationController {
+            modal.detents = [.medium()]
         }
+        present(modalVc, animated: true)
+    }
+    
+    func readMovieDetail() {
+        // 특정 영화의 상세 정보 가져오기
+        NetworkManager.shared.fetchMovieDetail(movieId: 1022789) { movieDetail in
+            if let movieDetail = movieDetail {
+                DispatchQueue.main.async {
+                    self.movieInfoView.movieTitleLabel.text = movieDetail.title
+                    self.movieInfoView.openingDateLabel.text = movieDetail.releaseDate
+                    self.movieInfoView.descriptionLabel.text = movieDetail.overview
+                    self.movieInfoView.runningTimeLabel.text = "\(movieDetail.runtime)분"
+                    self.movieInfoView.genreLabel.text = movieDetail.genres.map { $0.name }.joined(separator: "/")
+                    if let posterPath = movieDetail.posterPath {
+                        let fullPosterUrl = "https://image.tmdb.org/t/p/w500/\(posterPath)"
+                        self.loadImage(from: fullPosterUrl)
+                        //            NetworkManager.shared.loadImage(from: fullPosterUrl) { image in
+                        //              self.movieInfoView.movieImageView.image = image
+                        //            }
+                    }
+                }
+            }
+        }
+    }
+    //MARK: -다운로드한 이미지를 movieInfoView.movieImageView에 설정
+    func loadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        // 네트워크에서 이미지 다운로드
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error loading image: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                print("Error: Unable to load image data")
+                return
+            }
+            
+            // 메인 스레드에서 이미지 뷰 업데이트
+            DispatchQueue.main.async {
+                self.movieInfoView.movieImageView.image = image
+            }
+        }.resume()
     }
 }
