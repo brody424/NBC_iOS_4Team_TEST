91bbe2d8f6729b86eb4bbac8b015a2f8415474ed
jjoohee <leejh950417@naver.com>
2024-07-25 23:04:02 +0900

    feat: searchView ui 수정 및 상세페이지로 연결 완료 # 수정 필요 : searchView와 뷰 컨트롤러 한번더 정리 하고 searchView 셀 파일로 셀이동 해야함

--- MovieReservation_4Team/Controller/SearchController.swift
+++ MovieReservation_4Team/Controller/SearchController.swift
@@ -1,37 +1,41 @@
 import UIKit
 
-class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
-    
+import UIKit
+
+class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, MovieCollectionViewCellDelegate {
+
     private let searchView = SearchView()
     private var searchResults: [Movie] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.title = "Search"
 
         view.addSubview(searchView)
         searchView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
+
         searchView.searchResultsCollectionView.dataSource = self
         searchView.searchResultsCollectionView.delegate = self
         searchView.searchBar.delegate = self
+        searchView.searchResultsCollectionView.delegate = self // Set delegate for MovieCollectionViewCellDelegate
 
+        // Assign delegate to SearchView
+        searchView.cellDelegate = self
     }
-    
+
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
-            searchResults = [] 
+            searchResults = []
         } else {
             fetchMovies(with: searchText)
         }
         searchView.searchResultsCollectionView.reloadData()
     }
-    
+
     func fetchMovies(with query: String) {
-        // 영화 가져오기 구현 예시
+        // Example movie fetching implementation
         NetworkManager.shared.searchMovies(query: query, page: 1) { [weak self] movies in
             guard let self = self else { return }
             self.searchResults = movies ?? []
@@ -40,25 +44,41 @@ class SearchViewController: UIViewController, UICollectionViewDataSource, UIColl
             }
         }
     }
-    
-    // MARK: - 컬렉션뷰 데이터 소스
-    
+
+    // MARK: - UICollectionViewDataSource Methods
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return searchResults.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
         let movie = searchResults[indexPath.item]
         cell.configure(with: movie)
+        cell.delegate = self // Set the delegate for cell
         return cell
     }
-    
-    // MARK: - 딜리게이트레이아웃
-    
+
+    // MARK: - UICollectionViewDelegateFlowLayout Methods
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (collectionView.bounds.width - 16) / 2
+        let width = (collectionView.bounds.width - 24) / 2
         let height = width * 1.5
         return CGSize(width: width, height: height)
+    }
+
+    // MARK: - MovieCollectionViewCellDelegate Methods
+
+    func didTapImageView(cell: MovieCollectionViewCell) {
+        guard let indexPath = searchView.searchResultsCollectionView.indexPath(for: cell) else { return }
+        let movie = searchResults[indexPath.item]
+        showMovieDetailViewController(for: movie)
+    }
+
+    private func showMovieDetailViewController(for movie: Movie) {
+        let movieDetailVC = MovieInfoViewController()
+        movieDetailVC.readMovieDetail(movieID: movie.id)
+        movieDetailVC.modalPresentationStyle = .pageSheet
+        present(movieDetailVC, animated: true, completion: nil)
     }
 }
