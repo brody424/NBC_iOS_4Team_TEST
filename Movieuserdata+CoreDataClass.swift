7beea60cf88ce034d7216f0e5289636e152dc5ca
Jeom1028 <apam144@gmail.com>
2024-07-25 14:20:53 +0900

    feat: #12 - mypageview coredata 추가

---
+++ Movieuserdata+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movieuserdata+CoreDataClass.swift
+//  MovieReservation_4Team
+//
+//  Created by t2023-m0117 on 7/25/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movieuserdata)
+public class Movieuserdata: NSManagedObject {
+
+}
